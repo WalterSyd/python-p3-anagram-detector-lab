327f7fa76816f8bfa9893ce28952ca87d1b26179
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-09-05 18:03:59 +0300

    Completed test implementation by code

--- lib/anagram.py
+++ lib/anagram.py
@@ -2,9 +2,16 @@
 
 class Anagram:
     def __init__(self, word):
-        self.word = word
+        #handle case sensitivity
+        self.word = word.lower()
 
     def match(self, words):
         #sort letters of original word
-        
-      
+        sorted_word = sorted(self.word)
+        #loop through words and return anagrams if they exist else return empty list
+        matches = [word for word in words if sorted(word.lower()) == sorted_word]
+        return matches
+
+result = Anagram("listen")
+answer = result.match(["enlists", "google", "banana"])
+print(answer)
